a8c116c598bac295b0de71448af922e9dfe4435c
Admin <Admin@DESKTOP-VB8P9QU>
2025-11-23 10:41:16 +0700

    update

---
+++ Phan_GUI/QuenMatKhau.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace DO_AN_BMCSDL.Phan_GUI
+{
+    partial class FormQuenMatKhau
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormQuenMatKhau));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblYeuCauThanhCong = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lnkQuayLaiDangNhap = new System.Windows.Forms.LinkLabel();
+            this.txtThongTinTaiKhoan = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnQuenMatKhau = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.lblYeuCauThanhCong);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.lnkQuayLaiDangNhap);
+            this.panel1.Controls.Add(this.btnQuenMatKhau);
+            this.panel1.Controls.Add(this.txtThongTinTaiKhoan);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(768, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(745, 661);
+            this.panel1.TabIndex = 2;
+            // 
+            // lblYeuCauThanhCong
+            // 
+            this.lblYeuCauThanhCong.BackColor = System.Drawing.Color.Lime;
+            this.lblYeuCauThanhCong.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblYeuCauThanhCong.ForeColor = System.Drawing.Color.White;
+            this.lblYeuCauThanhCong.Location = new System.Drawing.Point(513, 614);
+            this.lblYeuCauThanhCong.Name = "lblYeuCauThanhCong";
+            this.lblYeuCauThanhCong.Size = new System.Drawing.Size(220, 38);
+            this.lblYeuCauThanhCong.TabIndex = 15;
+            this.lblYeuCauThanhCong.Text = "Yêu cầu thành công";
+            this.lblYeuCauThanhCong.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblYeuCauThanhCong.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(188, 180);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(204, 23);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Nhập thông tin tài khoản";
+            // 
+            // lnkQuayLaiDangNhap
+            // 
+            this.lnkQuayLaiDangNhap.AutoSize = true;
+            this.lnkQuayLaiDangNhap.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lnkQuayLaiDangNhap.Location = new System.Drawing.Point(294, 580);
+            this.lnkQuayLaiDangNhap.Name = "lnkQuayLaiDangNhap";
+            this.lnkQuayLaiDangNhap.Size = new System.Drawing.Size(200, 23);
+            this.lnkQuayLaiDangNhap.TabIndex = 5;
+            this.lnkQuayLaiDangNhap.TabStop = true;
+            this.lnkQuayLaiDangNhap.Text = "Quay lại trang đăng nhập";
+            // 
+            // txtThongTinTaiKhoan
+            // 
+            this.txtThongTinTaiKhoan.BackColor = System.Drawing.SystemColors.Control;
+            this.txtThongTinTaiKhoan.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtThongTinTaiKhoan.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtThongTinTaiKhoan.Location = new System.Drawing.Point(192, 206);
+            this.txtThongTinTaiKhoan.Multiline = true;
+            this.txtThongTinTaiKhoan.Name = "txtThongTinTaiKhoan";
+            this.txtThongTinTaiKhoan.Size = new System.Drawing.Size(400, 50);
+            this.txtThongTinTaiKhoan.TabIndex = 1;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.White;
+            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(745, 121);
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(762, 661);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnQuenMatKhau
+            // 
+            this.btnQuenMatKhau.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnQuenMatKhau.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnQuenMatKhau.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnQuenMatKhau.ForeColor = System.Drawing.Color.White;
+            this.btnQuenMatKhau.Location = new System.Drawing.Point(306, 522);
+            this.btnQuenMatKhau.Name = "btnQuenMatKhau";
+            this.btnQuenMatKhau.Size = new System.Drawing.Size(188, 43);
+            this.btnQuenMatKhau.TabIndex = 4;
+            this.btnQuenMatKhau.Text = "Quên mật khẩu";
+            this.btnQuenMatKhau.UseVisualStyleBackColor = false;
+            // 
+            // FormQuenMatKhau
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1513, 661);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "FormQuenMatKhau";
+            this.ShowIcon = false;
+            this.Text = "Quên mật khẩu";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblYeuCauThanhCong;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.LinkLabel lnkQuayLaiDangNhap;
+        private System.Windows.Forms.Button btnQuenMatKhau;
+        private System.Windows.Forms.TextBox txtThongTinTaiKhoan;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
